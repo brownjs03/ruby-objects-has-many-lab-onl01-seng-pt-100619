c1043c7aa9a2d29033c8ac05079b8d3276930f45
Jeffrey Brown <brownjs03@gmail.com>
2019-11-15 05:54:21 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
   
   attr_accessor :name 
   
+  @@post_count = 0 
+  
   def initialize(name)
     @name = name
   end 
@@ -12,11 +14,17 @@ class Author
   
   def add_post(post)
     post.author = self 
+    @@post_count += 1 
   end 
   
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     add_post(post)
+    @@post_count += 1 
+  end 
+  
+  def self.post_count
+    @@post_count
   end 
   
 end 
